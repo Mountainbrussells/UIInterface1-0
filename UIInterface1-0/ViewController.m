546ff8ae42e6b861ab48ee2abedd9f49661b6073
Mountainbrussells <brussell@Bens-MacBook-Pro.local>
2015-05-05 15:16:10 -0600

    Initial Commit

---
+++ UIInterface1-0/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIInterface1-0
+//
+//  Created by Ben Russell on 5/5/15.
+//  Copyright (c) 2015 Ben Russell. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ UIInterface1-0/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIInterface1-0
+//
+//  Created by Ben Russell on 5/5/15.
+//  Copyright (c) 2015 Ben Russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
